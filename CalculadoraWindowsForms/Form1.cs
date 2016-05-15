9f710adc0a338aa1833bfe06c471f6656861dc49
ronaldalbertjr <ronaldalbert1609@gmail.com>
2016-05-15 18:19:42 -0300

    CalculatorNowFunctional
    IT'S WORKING !!! there is still improvements to be done, but the
    calculator is actually calculating

--- CalculadoraWindowsForms/Form1.cs
+++ CalculadoraWindowsForms/Form1.cs
@@ -13,8 +13,9 @@ namespace CalculadoraWindowsForms
     public partial class Form1 : Form
     {
         string Text;
-        int number1 = 0;
-        int number2 = 0;
+        string number1, number2;
+        bool par1 = false;
+        int rn1, rn2;
         public Form1()
         {
             InitializeComponent();
@@ -27,170 +28,199 @@ namespace CalculadoraWindowsForms
 
         private void button11_Click(object sender, EventArgs e)
         {
-            Tela.Text += "+";
-            Text = "Soma";
-
+            if (!par1)
+            {
+                Tela.Text += "+";
+                Text = "Soma";
+                par1 = true;
+            }
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            Tela.Text += "-";
-            Text = "Subtra";
+            if (!par1)
+            {
+                Tela.Text += "-";
+                Text = "Subtra";
+                par1 = true;
+            }
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            Tela.Text += "*";
-            Text = "Multipli";
+            if (!par1)
+            {
+                Tela.Text += "*";
+                Text = "Multipli";
+                par1 = true;
+            }
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            Tela.Text += "/";
-            Text = "Divisão";
+            if (!par1)
+            {
+                Tela.Text += "/";
+                Text = "Divisão";
+                par1 = true;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if(Text == null && number1 == 0)
+            if(!par1)
             {
-                number1 = 9;
+                number1 += "9";
+                Tela.Text += 9;
             }
-            else if(Text != null && number1 != 0)
+            else 
             {
-                number2 = 9;
+                number2 += "9";
                 Tela.Text += 9;
             }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (Text == null && number1 == 0)
+            if (!par1)
             {
-                number1 = 8;
+                number1 += "8";
+                Tela.Text += 8;
             }
-            else if (Text != null && number1 != 0)
+            else 
             {
-                number2 = 8;
+                number2 = "8";
                 Tela.Text += 8;
             }
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            if (Text == null && number1 == 0)
+            if (!par1)
             {
-                number1 = 7;
+                number1 += "7";
+                Tela.Text += 7;
             }
-            else if (Text != null && number1 != 0)
+            else
             {
-                number2 = 7;
+                number2 = "7";
                 Tela.Text += 7;
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (Text == null && number1 == 0)
+            if (!par1)
             {
-                number1 = 6;
+                number1 += "6";
+                Tela.Text += 6;
             }
-            else if (Text != null && number1 != 0)
+            else
             {
-                number2 = 6;
+                number2 = "6";
                 Tela.Text += 6;
             }
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            if (Text == null && number1 == 0)
+            if (!par1)
             {
-                number1 = 5;
+                number1 += "5";
+                Tela.Text += 5;
             }
-            else if (Text != null && number1 != 0)
+            else
             {
-                number2 = 5;
+                number2 += "5";
                 Tela.Text += 5;
             }
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            if (Text == null && number1 == 0)
+            if (!par1)
             {
-                number1 = 4;
+                number1 += "4";
+                Tela.Text += 4;
             }
-            else if (Text != null && number1 != 0)
+            else
             {
-                number2 = 4;
+                number2 += "4";
                 Tela.Text += 4;
             }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (Text == null && number1 == 0)
+            if (!par1)
             {
-                number1 = 3;
+                number1 += "3";
+                Tela.Text += 3;
             }
-            else if (Text != null && number1 != 0)
+            else
             {
-                number2 = 3;
+                number2 += "3";
                 Tela.Text += 3;
             }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (Text == null && number1 == 0)
+            if (!par1)
             {
-                number1 = 2;
+                number1 += "2";
+                Tela.Text += 8;
             }
-            else if (Text != null && number1 != 0)
+            else
             {
-                number2 = 2;
+                number2 += "2";
                 Tela.Text += 2;
             }
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            if (Text == null && number1 == 0)
+            if (!par1)
             {
-                number1 = 1;
+                number1 += "1";
+                Tela.Text += 1;
             }
-            else if (Text != null && number1 != 0)
+            else
             {
-                number2 = 1;
+                number2 += "1";
                 Tela.Text += 1;
             }
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
+            rn1 = Convert.ToInt32(number1);
+            rn2 = Convert.ToInt32(number2);
             switch(Text)
             {
                 case "Soma":
-                    Tela.Text = Convert.ToString(number1 + number2);
+                    Tela.Text = Convert.ToString(rn1 + rn2);
                     break;
                 case "Subtra":
-                    Tela.Text = Convert.ToString(number1 + number2);
+                    Tela.Text = Convert.ToString(rn1 - rn2);
                     break;
                 case "Multipli":
-                    Tela.Text = Convert.ToString(number1 + number2);
+                    Tela.Text = Convert.ToString(rn1 * rn2);
                     break;
                 case "Divisão":
-                    Tela.Text = Convert.ToString(number1 + number2);
+                    Tela.Text = Convert.ToString(rn1 / rn2);
                     break;
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            number1 = 0;
-            number2 = 0;
-            Text = null;
+            rn1 = 0;
+            rn2 = 0;
+            number1 = null;
+            number2 = null;
+            Tela.Text = null;
+            par1 = false;
         }
     }
 }
